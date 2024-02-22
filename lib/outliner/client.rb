537a97156125705d0cff629481a0bfde494e26ee
dhx <dh.tx.dev@dhx.at>
2024-02-22 18:20:20 +0100

    Fixes the exportAll -> export_all outline API change
    As until now the client implementation replaced single underscores with a dot
    this was not compatible with underscores in the API URIs.
    For this reason now double underscores in the method name are replaced with a
    dot instead.

--- lib/outliner/client.rb
+++ lib/outliner/client.rb
@@ -11,17 +11,18 @@ module Outliner
     end
 
     def find_or_create_collection(name)
-      collections = self.collections_list(limit: 100)['data']
+      collections = self.collections__list(limit: 100)['data']
       collections.filter!{|c|c['name'] == name}
       if collections.size >= 1
         collections[0]['id']
       else
-        self.collections_create(name: name, description: 'Imported Collection')['data']['id']
+        self.collections__create(name: name, description: 'Imported Collection')['data']['id']
       end
     end
 
     def method_missing(method_name, params = {})
-      method_name = '/' + method_name.to_s.sub('_', '.')
+      method_name = '/' + method_name.to_s.sub('__', '.')
+      puts method_name
       body = {token: @token}.merge(params).to_json
       options = {
         body: body,
